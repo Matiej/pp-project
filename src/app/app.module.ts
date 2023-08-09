ec0946dda68cc7f9feb3524b2df2164e36866cc3
matiej <maciek.wojcik179@gmail.com>
2023-08-09 15:56:55 +0200

    app module add ALL new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,27 @@ import { AppComponent } from './app.component';
 import { LearningMixComponent } from './learning-mix/learning-mix.component';
 import { LearningMixModule } from './learning-mix/learning-mix.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BookApiComponent } from './book-api/book-api.component';
+import { HeaderComponent } from './header/header.component';
+import { BooksComponent } from './book-api/books/books.component';
+import { BookListComponent } from './book-api/books/book-list/book-list.component';
+import { BookDetailComponent } from './book-api/books/book-detail/book-detail.component';
+import { WishListComponent } from './wish-list/wish-list.component';
+import { WishItemComponent } from './wish-list/wish-item/wish-item.component';
+import { WishEditComponent } from './wish-list/wish-edit/wish-edit.component';
  
 @NgModule({
   declarations: [
     AppComponent,
     LearningMixComponent,
-    
+    BookApiComponent,
+    HeaderComponent,
+    BooksComponent,
+    BookListComponent,
+    BookDetailComponent,
+    WishListComponent,
+    WishItemComponent,
+    WishEditComponent
   ],
   imports: [
     BrowserModule,
