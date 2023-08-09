54d7a86ddadeeb944a356c2abd62db9ee548f975
matiej <maciek.wojcik179@gmail.com>
2023-08-09 14:26:21 +0200

    general bookAPI component

---
+++ src/app/book-api/book-api.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-api',
+  templateUrl: './book-api.component.html',
+  styleUrls: ['./book-api.component.css']
+})
+export class BookApiComponent {
+
+}
