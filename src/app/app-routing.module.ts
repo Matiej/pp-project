02992cb5cd0394a24ef6363ded59dbc0f613e7a3
matiej <maciek.wojcik179@gmail.com>
2023-08-10 10:04:33 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LearningMixComponent } from './learning-mix/learning-mix.component';
+import { WishListComponent } from './wish-list/wish-list.component';
+import { BooksComponent } from './book-api/books/books.component';
 
 const routes: Routes = [
   {
     path: 'learning-mix',
     component: LearningMixComponent,
   },
+  {
+    path: 'whish-list',
+    component: WishListComponent
+  },
+  {
+    path: 'books',
+    component: BooksComponent
+  }
 ];
 
 @NgModule({
