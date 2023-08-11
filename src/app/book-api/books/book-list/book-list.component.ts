8ebe731f7fc58ea878477234b04ea5aad72fe6bf
matiej <maciek.wojcik179@gmail.com>
2023-08-11 15:48:22 +0200

    book list modification to handle new object and search

--- src/app/book-api/books/book-list/book-list.component.ts
+++ src/app/book-api/books/book-list/book-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { Book } from '../model/book.model';
 import { BooksService } from '../service/books.service';
 
@@ -7,15 +13,40 @@ import { BooksService } from '../service/books.service';
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css'],
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnChanges {
   books: Book[] = [];
+  @Input()
+  titleSearchQuery: string = '';
+
   constructor(private booksService: BooksService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (
+      changes['titleSearchQuery'] &&
+      changes['titleSearchQuery'].currentValue
+    ) {
+      this.searchForBooksByTitle(this.titleSearchQuery);
+    }
+  }
 
   ngOnInit(): void {
     this.loadBooks();
   }
 
   private loadBooks(): void {
-    this.books = this.booksService.fetchBooks();
+    this.books.push(...this.booksService.fetchBooks());
+  }
+
+  private searchForBooksByTitle(title: string): void {
+    this.booksService.searchBooksByTitle(title).subscribe(
+      (books: Book[]) => {
+        if (books.length > 0) {
+          this.books.push(...books);
+        }
+      },
+      (error: any) => {
+        console.log('An error occurred: ' + error);
+      }
+    );
   }
 }
