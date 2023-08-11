122fd42a70f06c35a334d7b8b66c904add0ad033
matiej <maciek.wojcik179@gmail.com>
2023-08-11 15:41:19 +0200

    clean bookApi module html

--- src/app/book-api/book-api.component.ts
+++ src/app/book-api/book-api.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-book-api',
   templateUrl: './book-api.component.html',
-  styleUrls: ['./book-api.component.css']
+  styleUrls: ['./book-api.component.css'],
 })
 export class BookApiComponent {
 
