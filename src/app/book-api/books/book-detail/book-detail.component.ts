0b70ae7c977e5788f9833a39461539653f2b3306
matiej <maciek.wojcik179@gmail.com>
2023-08-14 15:56:59 +0200

    implement book detail component

--- src/app/book-api/books/book-detail/book-detail.component.ts
+++ src/app/book-api/books/book-detail/book-detail.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Book } from '../model/book.model';
 
 @Component({
   selector: 'app-book-detail',
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.css']
 })
-export class BookDetailComponent {
+export class BookDetailComponent implements OnInit {
+  
+  @Output()
+  closeDetails: EventEmitter<void> = new EventEmitter();
 
+  @Input()
+  incomingBook: Book | undefined;
+
+  ngOnInit(): void {
+   
+  }
+
+  onCloseClick(): void {
+    this.closeDetails.emit();
+  }
 }
