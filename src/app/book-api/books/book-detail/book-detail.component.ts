97d0b4ebf5e4f8c6e2003ee915901095e5793132
matiej <maciek.wojcik179@gmail.com>
2023-08-16 13:58:20 +0200

    bookdetatil html hanldes some data from compontent, small style changes and adjustments. testing and learing css

--- src/app/book-api/books/book-detail/book-detail.component.ts
+++ src/app/book-api/books/book-detail/book-detail.component.ts
@@ -1,24 +1,61 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Book } from '../model/book.model';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { BookDetailResponse } from './book-detail-response';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-book-detail',
   templateUrl: './book-detail.component.html',
-  styleUrls: ['./book-detail.component.css']
+  styleUrls: ['./book-detail.component.css', '../book-list/book-list.component.css'],
 })
-export class BookDetailComponent implements OnInit {
-  
+export class BookDetailComponent implements OnInit, OnChanges {
   @Output()
   closeDetails: EventEmitter<void> = new EventEmitter();
 
   @Input()
-  incomingBook: Book | undefined;
+  $incomingBookDetailResponse: Observable<BookDetailResponse> | undefined;
+  bookDetailResponse: BookDetailResponse | undefined;
 
-  ngOnInit(): void {
-   
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngchanges');
+    console.log(changes['$incomingBookDetailResponse'].currentValue);
+
+    if (
+      changes['$incomingBookDetailResponse'] &&
+      changes['$incomingBookDetailResponse'].currentValue
+    ) {
+      this.getBookDetail();
+    }
   }
 
   onCloseClick(): void {
     this.closeDetails.emit();
+  }
+
+  openLargeImage(largeSizeCoverurl: string | undefined) {
+    const width = 800; 
+    const height = 600;
+    const features = `width=${width},height=${height}`;
+    window.open(largeSizeCoverurl, '_blank', features);
+  }
+
+  private getBookDetail(): void {
+    this.$incomingBookDetailResponse?.subscribe(
+      (detailResponse: BookDetailResponse) => {
+        this.bookDetailResponse = detailResponse;
+      },
+      (error: any) => {
+        console.log('An error occurred during book detail fetch: ' + error);
+      }
+    );
   }
 }
