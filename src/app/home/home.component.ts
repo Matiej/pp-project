80f514125dee78352094cf46417680ddcfb34294
matiej <maciek.wojcik179@gmail.com>
2023-08-17 10:08:40 +0200

    add home component and some pictures

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  images = [
+    { path: 'assets/im1.jpg', imageName: 'BMW R1200RT 2014-2019' },
+    { path: 'assets/im2.jpg', imageName: 'BMW R1200RT 2010-2013' },
+    { path: 'assets/im3.jpg', imageName: 'Moto Guzzi V85TT 2019`' },
+    { path: 'assets/im4.jpg', imageName: 'Moto Guzzi V85TT 2021`' },
+  ];
+
+  description: string = 'A short description of the app.';
+  title: string = 'Maciej Wójcik traning Angular + TypeScript application.';
+}
