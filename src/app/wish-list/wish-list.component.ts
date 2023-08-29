7cf6c84b984f57aa8b983e6dff172605c1c10c90
matiej <maciek.wojcik179@gmail.com>
2023-08-29 16:30:38 +0200

    add remove notification if wish is removed

--- src/app/wish-list/wish-list.component.ts
+++ src/app/wish-list/wish-list.component.ts
@@ -11,15 +11,34 @@ import { WishType } from './wish-item/wish-type';
 })
 export class WishListComponent implements OnInit {
   wishItems: WishItem[] = [];
+  showToast: boolean = false;
 
   constructor(private wishSharedService: WishSharedService) {}
 
   ngOnInit(): void {
     this.wishItems.push(...this.createWish());
     this.wishSharedService.getWishList().subscribe((wishList: WishItem[]) => {
-      this.wishItems.push(...wishList);
-      this.wishItems.reverse();
+      console.log(wishList);
+      if (wishList && wishList.length > 0) {
+        console.log('inside if');
+        this.wishItems.push(...wishList);
+        this.wishItems.reverse();
+      }
     });
+
+    this.wishSharedService.refreshWishCounter(this.wishItems.length);
+  }
+
+  public onRemoveWish(index: number): void {
+    if (index >= 0 && index < this.wishItems.length) {
+      this.wishItems.splice(index, 1);
+      this.wishSharedService.refreshWishCounter(this.wishItems.length);
+      this.showToast = true;
+
+      setTimeout(() => {
+        this.showToast = false;
+      }, 3000);
+    }
   }
 
   private createWish(): WishItem[] {
