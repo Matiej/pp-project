3f5fa50b789ff2d2e4e03232fe1e0ca824a99220
matiej <maciek.wojcik179@gmail.com>
2023-08-31 13:15:47 +0200

    add visibility for search filed and button

--- src/app/book-api/books/books-serch-bar/books-serch-bar.component.ts
+++ src/app/book-api/books/books-serch-bar/books-serch-bar.component.ts
@@ -1,29 +1,46 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-books-serch-bar',
   templateUrl: './books-serch-bar.component.html',
   styleUrls: ['./books-serch-bar.component.css'],
 })
-export class BooksSerchBarComponent implements OnInit {
-  selectedCriteria: string = 'title';
+export class BooksSerchBarComponent {
+  // selectedCriterias: string = 'title';
+  private _selectedCriteria: string = 'title';
   selectSorting: string = 'new';
-  searchText: string = '';
- 
-  @Output()
-  searchRequest: EventEmitter<{ criteria: string; text: string; sorting: string }> =
-    new EventEmitter();
+  searchValue: string = '';
+  isSearchFiledDisabled: boolean = false;
+  searchFiledPlaceholder: string = 'Enter your search';
 
-  ngOnInit(): void {
-    console.log("ng")
+  @Output()
+  searchRequest: EventEmitter<{
+    criteria: string;
+    text: string;
+    sorting: string;
+  }> = new EventEmitter();
+
+  get selectedCriteria(): string {
+    return this._selectedCriteria;
+  }
+
+  set selectedCriteria(value: string) {
+    this._selectedCriteria = value;
+    if (this._selectedCriteria === 'subject') {
+      this.searchFiledPlaceholder =
+        'Subject search is not available temporarily';
+      this.isSearchFiledDisabled = true;
+    } else {
+      this.searchFiledPlaceholder = 'Enter your search';
+    }
+    this.searchValue = '';
   }
 
   onSearchClick(searchText: HTMLInputElement): void {
-    console.log(this.selectedCriteria);
     this.searchRequest.emit({
-      criteria: this.selectedCriteria,
+      criteria: this._selectedCriteria,
       text: searchText.value,
-      sorting: this.selectSorting
+      sorting: this.selectSorting,
     });
     searchText.value = '';
   }
