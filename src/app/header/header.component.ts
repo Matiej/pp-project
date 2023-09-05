5ac66f7110cd480804719b6b511ec3bdc552cd06
matiej <maciek.wojcik179@gmail.com>
2023-09-05 15:59:24 +0200

    header nav bar improovment

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WishSharedService } from '../shared/wish-shared.service';
+import { EXTERNAL_LINKS } from '../constants/external-links';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +10,8 @@ import { WishSharedService } from '../shared/wish-shared.service';
 export class HeaderComponent implements OnInit {
   collapsed = true;
   wishCounter: number = 0;
+  gitHubLink: string = EXTERNAL_LINKS.GITHUB_PROJECT_FRONTEND;
+  linkedInLink: string = EXTERNAL_LINKS.LINKEDIN_LINK;
 
   constructor(private wishSharedService: WishSharedService) {}
 
