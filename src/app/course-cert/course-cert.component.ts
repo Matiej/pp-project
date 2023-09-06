18e112c40990a5b88ab86cbb2071fbb65ad6a9a9
matiej <maciek.wojcik179@gmail.com>
2023-09-06 11:56:17 +0200

    create and implement cert curse component

---
+++ src/app/course-cert/course-cert.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ASSETS_PATHS } from '../constants/assets-paths';
+
+@Component({
+  selector: 'app-course-cert',
+  templateUrl: './course-cert.component.html',
+  styleUrls: ['./course-cert.component.css'],
+})
+export class CourseCertComponent implements OnInit {
+  certPngSrc: string = ASSETS_PATHS.COURSE_CERT_FILE_SOURCE;
+  readonly certTitle: string = 'Angular - The Complete Guide (2023 Edition)';
+
+  ngOnInit(): void {
+    console.log(this.certPngSrc);
+  }
+}
