d5968d0f4db6a99a9f4aaa656081347e1cc72111
matiej <maciek.wojcik179@gmail.com>
2023-09-08 16:14:06 +0200

    section7 directives implementation for test and learning

--- src/app/learning-mix/section7-directives/section7-directives.component.ts
+++ src/app/learning-mix/section7-directives/section7-directives.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-section7-directives',
   templateUrl: './section7-directives.component.html',
-  styleUrls: ['./section7-directives.component.css']
+  styleUrls: ['./section7-directives.component.css'],
 })
 export class Section7DirectivesComponent {
+  onlyOdd: boolean = false;
+  numbers: number[] = [1, 2, 3, 4, 5, 6, 7];
+  buttonName: string = 'Show only odd numbers';
+displaingNumbers: string = 'EVEN NUMERS ONLY';
 
+  onButtonClick(): void {
+    this.onlyOdd = !this.onlyOdd;
+    this.buttonName = !this.onlyOdd
+      ? 'Show only odd numbers'
+      : 'Show only even numbers';
+
+      this.displaingNumbers = !this.onlyOdd
+      ? 'EVEN NUMERS ONLY'
+      : 'ODD NUMERS ONLY';
+  }
 }
