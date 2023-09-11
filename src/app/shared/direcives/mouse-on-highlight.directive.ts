56ea3ba70f2e348095b704b6478c9ff1acc98818
matiej <maciek.wojcik179@gmail.com>
2023-09-11 16:51:39 +0200

    add mouse on directive with hostlistener - test and example. Very useful

---
+++ src/app/shared/direcives/mouse-on-highlight.directive.ts
@@ -0,0 +1,35 @@
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appMouseOnHighlight]',
+})
+export class MouseOnHighlightDirective implements OnInit {
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+  ngOnInit(): void {}
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    console.log('mouseovew');
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'yellow'
+    );
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+  }
+
+  @HostListener('mouseleave') mouseleave() {
+    console.log('mouseleave');
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'transparent'
+    );
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'red');
+  }
+}
