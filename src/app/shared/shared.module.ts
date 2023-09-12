fbb7a47f64ffc0c9e242e8e504cebe76168d55a5
matiej <maciek.wojcik179@gmail.com>
2023-09-12 14:09:01 +0200

    test implementation of Structural Directive like *ngIf

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { BetterCorrectHighlightDirective } from './direcives/better-correct-highlight.directive';
 import { MouseOnHighlightDirective } from './direcives/mouse-on-highlight.directive';
 import { HostbindingMouseOnHighlightDirective } from './direcives/hostbinding-mouse-on-highlight.directive';
+import { UnlessDirective } from './direcives/unless.directive';
 
 @NgModule({
   declarations: [
     BetterCorrectHighlightDirective,
     MouseOnHighlightDirective,
     HostbindingMouseOnHighlightDirective,
+    UnlessDirective,
   ],
   imports: [CommonModule],
   exports: [
     BetterCorrectHighlightDirective,
     MouseOnHighlightDirective,
     HostbindingMouseOnHighlightDirective,
+    UnlessDirective
   ],
 })
 export class SharedModule {}
