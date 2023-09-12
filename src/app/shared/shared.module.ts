e044f6d7b56972f6296a7899ba112cc411d79d30
matiej <maciek.wojcik179@gmail.com>
2023-09-12 13:19:08 +0200

    add mouse on direvtive based on hostbinding, another way. For me better than ref and rederer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BetterCorrectHighlightDirective } from './direcives/better-correct-highlight.directive';
 import { MouseOnHighlightDirective } from './direcives/mouse-on-highlight.directive';
+import { HostbindingMouseOnHighlightDirective } from './direcives/hostbinding-mouse-on-highlight.directive';
 
 @NgModule({
-  declarations: [BetterCorrectHighlightDirective, MouseOnHighlightDirective],
+  declarations: [
+    BetterCorrectHighlightDirective,
+    MouseOnHighlightDirective,
+    HostbindingMouseOnHighlightDirective,
+  ],
   imports: [CommonModule],
-  exports: [BetterCorrectHighlightDirective, MouseOnHighlightDirective],
+  exports: [
+    BetterCorrectHighlightDirective,
+    MouseOnHighlightDirective,
+    HostbindingMouseOnHighlightDirective,
+  ],
 })
 export class SharedModule {}
