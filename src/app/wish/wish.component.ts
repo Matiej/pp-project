26b9cd7180ea8d6f52d1d0211402078b72900f89
matiej <maciek.wojcik179@gmail.com>
2023-09-13 12:07:19 +0200

    add wish component, to refactor all wish section

---
+++ src/app/wish/wish.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wish',
+  templateUrl: './wish.component.html',
+  styleUrls: ['./wish.component.css']
+})
+export class WishComponent {
+isSpinner: boolean = false;
+
+}
