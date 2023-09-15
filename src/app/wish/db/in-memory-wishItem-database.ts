519184c3d4e68cc7a334375bc5a78c78d5233b3b
matiej <maciek.wojcik179@gmail.com>
2023-09-15 14:46:49 +0200

    move inmemory db to another package

---
+++ src/app/wish/db/in-memory-wishItem-database.ts
@@ -0,0 +1,41 @@
+import { WishItem } from '../wish-list/wish-item/wish-item-model';
+export class InMemoryWishItemDataBase {
+  private db: Map<string, WishItem> = new Map();
+
+  add(item: WishItem): WishItem | undefined {
+    if (isNaN(item.id) || typeof item.id === 'undefined') {
+      const nextId: number = parseInt(this.getCurrentId()) + 1;
+      item.id = nextId;
+      this.db.set(item.id.toString(), item);
+
+      return item;
+    } else {
+      this.db.set(item.id.toString(), item);
+
+      return item;
+    }
+  }
+
+  get(id: string): WishItem | undefined {
+    return this.db.get(id);
+  }
+
+  remove(id: string): boolean {
+    return this.db.delete(id);
+  }
+
+  listAll(): Array<WishItem> {
+    console.log(this.db.values());
+    return Array.from(this.db.values());
+  }
+
+  private getCurrentId(): string {
+    if (this.db.size < 1) {
+      return '0';
+    }
+    const sorted = Array.from(this.db.keys()).sort(
+      (a, b) => parseInt(b) - parseInt(a)
+    );
+    return sorted[0];
+  }
+}
