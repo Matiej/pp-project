aab8d30377ba4295e69cac7d11577b526891e5c7
matiej <maciek.wojcik179@gmail.com>
2023-09-19 13:38:16 +0200

    remove usless imports, add services to provider array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookApiComponent } from './book-api/book-api.component';
+import { BookDetailComponent } from './book-api/books/book-detail/book-detail.component';
+import { BookItemComponent } from './book-api/books/book-list/book-item/book-item.component';
+import { BookListComponent } from './book-api/books/book-list/book-list.component';
+import { BooksSerchBarComponent } from './book-api/books/books-serch-bar/books-serch-bar.component';
+import { BooksComponent } from './book-api/books/books.component';
+import { OpenlibraryApiService } from './book-api/openlibrary-api/service/openlibrary-api.service';
+import { CourseCertComponent } from './course-cert/course-cert.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 import { LearningMixComponent } from './learning-mix/learning-mix.component';
 import { LearningMixModule } from './learning-mix/learning-mix.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BookApiComponent } from './book-api/book-api.component';
-import { HeaderComponent } from './header/header.component';
-import { BooksComponent } from './book-api/books/books.component';
-import { BookListComponent } from './book-api/books/book-list/book-list.component';
-import { BookDetailComponent } from './book-api/books/book-detail/book-detail.component';
-import { WishItemComponent } from './wish/wish-list/wish-item/wish-item.component';
-import { WishEditComponent } from './wish/wish-edit/wish-edit.component';
-import { OpenlibraryApiService } from './book-api/openlibrary-api/service/openlibrary-api.service';
-import { BookItemComponent } from './book-api/books/book-list/book-item/book-item.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
-import { BooksSerchBarComponent } from './book-api/books/books-serch-bar/books-serch-bar.component';
-import { CourseCertComponent } from './course-cert/course-cert.component';
-import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { SharedModule } from './shared/shared.module';
-import { WishComponent } from './wish/wish.component';
+import { WishSharedService } from './shared/wish-shared.service';
+import { InMemoryDatabaseService } from './wish/service/in-memory-database.service';
+import { WishEditComponent } from './wish/wish-edit/wish-edit.component';
+import { WishItemComponent } from './wish/wish-list/wish-item/wish-item.component';
 import { WishListComponent } from './wish/wish-list/wish-list.component';
+import { WishComponent } from './wish/wish.component';
 
 @NgModule({
   declarations: [
@@ -52,7 +53,7 @@ import { WishListComponent } from './wish/wish-list/wish-list.component';
     NgxExtendedPdfViewerModule,
     SharedModule,
   ],
-  providers: [OpenlibraryApiService],
+  providers: [OpenlibraryApiService, WishSharedService, InMemoryDatabaseService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
