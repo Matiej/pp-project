51b6204cb5a8a61a4daf0d517ee38e859b2a05fc
matiej <maciek.wojcik179@gmail.com>
2023-09-20 15:13:42 +0200

    section9 implementation

--- src/app/learning-mix/section9-task-user/section9-task-user.component.ts
+++ src/app/learning-mix/section9-task-user/section9-task-user.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Section9TaskService } from './section9-task.service';
+import { TaskUser } from './task-user-model';
+import { TaskUserStatus } from './user-status-enum';
 
 @Component({
   selector: 'app-section9-task-user',
   templateUrl: './section9-task-user.component.html',
-  styleUrls: ['./section9-task-user.component.css']
+  styleUrls: ['./section9-task-user.component.css'],
 })
 export class Section9TaskUserComponent {
+  clickCounter: number = 0;
 
+  constructor(private section9TaskService: Section9TaskService) {
+    section9TaskService.incraseClickCounter.subscribe(() => {
+      this.clickCounter += 1;
+    });
+    this.preapreDefaultUsers();
+  }
+
+  private preapreDefaultUsers() {
+    const user1 = new TaskUser('Joh', TaskUserStatus.ACTIVE);
+    this.section9TaskService.saveUser(user1);
+    const user2 = new TaskUser('Mike', TaskUserStatus.ACTIVE);
+    this.section9TaskService.saveUser(user2);
+    const user3 = new TaskUser('Anna', TaskUserStatus.ACTIVE);
+    this.section9TaskService.saveUser(user3);
+    const user4 = new TaskUser('Julia', TaskUserStatus.INACTIVE);
+    this.section9TaskService.saveUser(user4);
+    const user5 = new TaskUser('Monika', TaskUserStatus.ACTIVE);
+    this.section9TaskService.saveUser(user5);
+    const user6 = new TaskUser('Franciesco', TaskUserStatus.INACTIVE);
+    this.section9TaskService.saveUser(user6);
+  }
 }
