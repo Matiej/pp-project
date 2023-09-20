a58ba5c452f3b4af63ee9b13fd9d3c628d5ac1a5
matiej <maciek.wojcik179@gmail.com>
2023-09-20 11:29:32 +0200

    add section9 task component

---
+++ src/app/learning-mix/section9-task-user/section9-task-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section9-task-user',
+  templateUrl: './section9-task-user.component.html',
+  styleUrls: ['./section9-task-user.component.css']
+})
+export class Section9TaskUserComponent {
+
+}
