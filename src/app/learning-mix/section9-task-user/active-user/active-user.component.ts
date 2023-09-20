f80a5d825e87486c329f1916ed83bdb7c75a7309
matiej <maciek.wojcik179@gmail.com>
2023-09-20 15:13:56 +0200

    active user impl

--- src/app/learning-mix/section9-task-user/active-user/active-user.component.ts
+++ src/app/learning-mix/section9-task-user/active-user/active-user.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Section9TaskService } from '../section9-task.service';
+import { TaskUser } from '../task-user-model';
+import { TaskUserStatus } from '../user-status-enum';
 
 @Component({
   selector: 'app-active-user',
   templateUrl: './active-user.component.html',
-  styleUrls: ['./active-user.component.css']
+  styleUrls: ['./active-user.component.css'],
 })
-export class ActiveUserComponent {
+export class ActiveUserComponent implements OnInit {
+  activeUsers: TaskUser[] = [];
+  userChnageStatusNotifications: EventEmitter<void> = new EventEmitter();
 
+  constructor(private section9TaskService: Section9TaskService) {
+    section9TaskService.refreshUserComponetnsList.subscribe(()=> {
+      this.subScribeUsers();
+    })
+  }
+
+  ngOnInit(): void {
+    this.subScribeUsers();
+  }
+
+  onSetToInactive(user: TaskUser) {
+    user.status = TaskUserStatus.INACTIVE;
+    const savedUser = this.section9TaskService.saveUser(user);
+    if (savedUser.status === TaskUserStatus.INACTIVE) {
+      console.log('INACTIVE');
+      this.section9TaskService.onUserChangeStatusActions();
+    }
+  }
+
+  private subScribeUsers(): void {
+    console.log('subscribing active users');
+    this.section9TaskService
+      .getTaskUsers(TaskUserStatus.ACTIVE)
+      .subscribe((taskUsers: TaskUser[]) => {
+        this.activeUsers = taskUsers;
+      });
+  }
 }
