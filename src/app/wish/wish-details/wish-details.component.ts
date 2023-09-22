f4b1d140c1601e050b391b36696ec6c8bdcf653a
matiej <maciek.wojcik179@gmail.com>
2023-09-22 13:09:00 +0200

    refactor wishedit component to move reomve wishbutton from list to details

--- src/app/wish/wish-details/wish-details.component.ts
+++ src/app/wish/wish-details/wish-details.component.ts
@@ -6,7 +6,7 @@ import { WishItem } from '../wish-list/wish-item/wish-item-model';
 @Component({
   selector: 'app-wish-details',
   templateUrl: './wish-details.component.html',
-  styleUrls: ['./wish-details.component.css'],
+  styleUrls: ['./wish-details.component.css', '../wish.component.css'],
 })
 export class WishDetailsComponent implements OnInit, OnDestroy {
   wishItem: WishItem | undefined;
@@ -19,30 +19,39 @@ export class WishDetailsComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this._destroy$))
       .subscribe((wishItem) => {
         if (wishItem) {
+          console.log('subscirbe wishItemDetailSend wishIs present')
           this.wishItem = wishItem;
         } else {
-          this.onCloseClick();
+          console.log('subscirbe wishItemDetailSend undefined')
           this.wishItem = undefined;
         }
       });
-      this.wishSharedService.wishItenDetailRemove
-      .pipe(takeUntil(this._destroy$))
-      .subscribe((wishId: number) => {
-        if(this.wishItem && this.wishItem.id === wishId) {
-          this.onCloseClick();
-        }
-      })
+    // this.wishSharedService.wishItenDetailRemove
+    //   .pipe(takeUntil(this._destroy$))
+    //   .subscribe((wishId: number) => {
+    //     if (this.wishItem && this.wishItem.id === wishId) {
+    //       this.onCloseClick();
+    //     }
+    //   });
   }
 
   openLargeImage(arg0: any) {
     throw new Error('Method not implemented.');
   }
   onCloseClick() {
+    this.wishItem = undefined;
     this.wishSharedService.onCloseWishDetailClick();
   }
 
   ngOnDestroy(): void {
     this._destroy$.next();
     this._destroy$.complete();
+  }
+
+  onRemoveWish() {
+    if (this.wishItem) {
+      this.wishSharedService.removeWishItem(this.wishItem.id);
+      this.onCloseClick();
+    }
   }
 }
