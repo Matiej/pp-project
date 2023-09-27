d6bea42ee72c7efd565f7a75611800ed8df8a144
matiej <maciek.wojcik179@gmail.com>
2023-09-27 13:13:53 +0200

    add section11 routing compontents to test and learn

--- src/app/learning-mix/section11-routing/navo-section11/navo-section11.component.ts
+++ src/app/learning-mix/section11-routing/navo-section11/navo-section11.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonLabelName } from 'src/app/shared/button-name';
+import { LearningMixSharedService } from '../../learning-mix-shared.service';
 
 @Component({
   selector: 'app-navo-section11',
   templateUrl: './navo-section11.component.html',
-  styleUrls: ['./navo-section11.component.css']
+  styleUrls: ['./navo-section11.component.css'],
 })
 export class NavoSection11Component {
+  constructor(
+    private router: Router,
+    private sharedLearningService: LearningMixSharedService
+  ) {}
 
+  onOptions1Click() {
+    console.log('onOptions1Click');
+    this.sharedLearningService.turnOnSection(ButtonLabelName.SECTION11_ROUTING);
+    this.router.navigate(['learning-mix/sec11/options1']);
+  }
+
+  onOptions2Click() {
+    this.sharedLearningService.turnOnSection(ButtonLabelName.SECTION11_ROUTING);
+  }
 }
