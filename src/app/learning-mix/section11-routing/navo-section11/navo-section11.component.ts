41a8f4a4aea0ca229003a3f7a15a9651f6bf9621
matiej <maciek.wojcik179@gmail.com>
2023-09-28 14:20:07 +0200

    add guard service - implementation later, check router from angular-router

--- src/app/learning-mix/section11-routing/navo-section11/navo-section11.component.ts
+++ src/app/learning-mix/section11-routing/navo-section11/navo-section11.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ButtonLabelName } from 'src/app/shared/button-name';
 import { LearningMixSharedService } from '../../learning-mix-shared.service';
 
@@ -11,13 +11,15 @@ import { LearningMixSharedService } from '../../learning-mix-shared.service';
 export class NavoSection11Component {
   constructor(
     private router: Router,
-    private sharedLearningService: LearningMixSharedService
+    private sharedLearningService: LearningMixSharedService,
+    private route: ActivatedRoute
   ) {}
 
   onOptions1Click() {
     console.log('onOptions1Click');
     this.sharedLearningService.turnOnSection(ButtonLabelName.SECTION11_ROUTING);
-    this.router.navigate(['learning-mix/sec11/options1']);
+    // this.router.navigate(['learning-mix/sec11/options1']);
+    this.router.navigate(['options1'], { relativeTo: this.route});
   }
 
   onOptions2Click() {
