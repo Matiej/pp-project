5c7878739d239939f1534b49909c1a29bfbf2e9d
matiej <maciek.wojcik179@gmail.com>
2023-09-29 12:45:47 +0200

    implements details for user.

--- src/app/user/user-details/user-details.component.ts
+++ src/app/user/user-details/user-details.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { User } from '../user-model';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+  styleUrls: ['./user-details.component.css'],
 })
 export class UserDetailsComponent {
+  user?: User;
 
+  onCloseClick() {
+    console.log('onCloseClick');
+  }
+
+  onRemoveWish() {
+    console.log('onRemoveWish');
+  }
 }
