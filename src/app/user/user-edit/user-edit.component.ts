c241e5521b461776dfefa96b1c765d920c4c60a1
matiej <maciek.wojcik179@gmail.com>
2023-10-02 16:34:22 +0200

    implementation of userEdit Component. Need to implement remove method.

--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UserDatabaseService } from '../service/user-database.service';
+import { UserSharedService } from '../service/user-shared.service';
+import { User } from '../user-model';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css', '../user.component.css'],
 })
-export class UserEditComponent {
+export class UserEditComponent implements OnInit, OnDestroy {
+  showToast: boolean = false;
+  userEditToastMessage: string = '';
 
+  user?: User;
+  paramSubscription?: Subscription;
+  userSubscription?: Subscription;
+  userForm!: FormGroup;
+  userEditTitle: string = 'User Edit Section';
+
+  constructor(
+    private fb: FormBuilder,
+    private userDatabaseService: UserDatabaseService,
+    private route: ActivatedRoute,
+    private userSharedSevice: UserSharedService
+  ) {}
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      birthYear: ['', Validators.required],
+    });
+
+    this.paramSubscription = this.route.params.subscribe((params: Params) => {
+      const userId: string = params['id'];
+      if (userId && !Number.isNaN(userId)) {
+        console.log(Number.isNaN(userId));
+        this.userDatabaseService
+          .findById(Number.parseFloat(userId))
+          .subscribe((data) => {
+            if (data) {
+              this.user = data;
+              this.fillOutForm(data);
+            }
+          });
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription?.unsubscribe();
+    this.paramSubscription?.unsubscribe();
+  }
+
+  onSaveButtonClick() {
+    console.log(this.userForm);
+    const formData = this.userForm.value;
+    let userToSve = new User(
+      formData.name,
+      formData.lastName,
+      formData.email,
+      formData.bithYear
+    );
+    if (this.user!.id) {
+      userToSve.id = this.user!.id;
+    }
+    this.userDatabaseService.saveUser(userToSve);
+    this.userSharedSevice.updateUserDataNotify();
+  }
+
+  private fillOutForm(user: User): void {
+    let valueToSet = {
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      birthYear: user.birthYear,
+    };
+    this.userForm.setValue(valueToSet);
+  }
 }
