213336ec0a923370ec0d9fc7c95c0832d0017de1
matiej <maciek.wojcik179@gmail.com>
2023-10-02 11:55:40 +0200

    create outletTest component to play outlets and all aroudn

---
+++ src/app/named-outlet-test/named-outlet-test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-named-outlet-test',
+  templateUrl: './named-outlet-test.component.html',
+  styleUrls: ['./named-outlet-test.component.css']
+})
+export class NamedOutletTestComponent {
+
+}
