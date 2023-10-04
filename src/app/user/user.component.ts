f6c26fd1c9d248d7c07e7fa563597ae0b68b4d9f
matiej <maciek.wojcik179@gmail.com>
2023-10-04 13:59:06 +0200

    add toastMessages to userComponent

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit } from '@angular/core';
+import { UserSharedService } from './service/user-shared.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent {
+export class UserComponent implements OnInit, OnDestroy {
   readonly userComponentTitle: string = 'User Section';
   isSpinner: boolean = false;
-  isUseretail: boolean = true;
-  isNewUser: boolean = false;
+  userToastMessage: string = '';
+  showToast: boolean = false;
+  toastMessageClass: string = 'success-toast';
+  private _toastEmiter: EventEmitter<any> = new EventEmitter();
 
-  onAddNewUser() {
-    this.isNewUser = !this.isNewUser;
+  constructor(private userSharedService: UserSharedService) {}
+
+  ngOnInit(): void {
+    this._toastEmiter = this.userSharedService.toastMessageEmiter;
+    this._toastEmiter.subscribe(
+      (toast: { message: string; styleClass: string }) => {
+        this.showToastMessage(toast.message, 3000, toast.styleClass);
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._toastEmiter.unsubscribe();
+  }
+
+  private showToastMessage(
+    message: string,
+    timeout: number,
+    messageStyle: string
+  ): void {
+    this.userToastMessage = message;
+    this.showToast = true;
+    this.toastMessageClass = messageStyle;
+    setTimeout(() => {
+      this.showToast = false;
+      this.userToastMessage = '';
+      this.toastMessageClass = '';
+    }, timeout);
   }
 }
