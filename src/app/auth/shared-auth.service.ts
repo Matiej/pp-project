f5b4330ea0ddcc4d26054aa5014b7d13c419422d
matiej <maciek.wojcik179@gmail.com>
2023-10-05 13:57:04 +0200

    add shardService for login and auth

---
+++ src/app/auth/shared-auth.service.ts
@@ -0,0 +1,19 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SharedAuthService {
+  private _loginNotification: EventEmitter<boolean> = new EventEmitter();
+
+  constructor() {}
+
+  public get loginNotification(): EventEmitter<boolean> {
+    return this._loginNotification;
+  }
+
+  public userLoggedINNotification(): void {
+    this._loginNotification.emit(true)
+ 
+  }
+}
