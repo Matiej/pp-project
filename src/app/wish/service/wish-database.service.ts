68be903bb8ab0b3be82c2075a56450797fc7515f
matiej <maciek.wojcik179@gmail.com>
2023-10-10 14:53:34 +0200

    new wish database service file provided

---
+++ src/app/wish/service/wish-database.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { InMemoryWishItemDataBase } from '../db/in-memory-wishItem-database';
+import { WishItem } from '../wish-list/wish-item/wish-item-model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WishDatabaseService {
+  private _whisItemDatabase!: InMemoryWishItemDataBase;
+
+  constructor() {
+    this._whisItemDatabase = new InMemoryWishItemDataBase();
+  }
+
+  saveWishItem(item: WishItem): WishItem | undefined {
+    return this._whisItemDatabase.add(item);
+  }
+
+  findById(id: number): Observable<WishItem | undefined> {
+    return of(this._whisItemDatabase.get(id.toString()));
+  }
+
+  findAll(): Observable<WishItem[]> {
+ 
+    return of(this._whisItemDatabase.listAll());
+  }
+
+  removeById(id: number): boolean {
+    return this._whisItemDatabase.remove(id.toString());
+  }
+
+  getNumberOfItems(): number {
+    return this._whisItemDatabase.listAll().length;
+  }
+}
