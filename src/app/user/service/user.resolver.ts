ed603f124fce646502c4965bd9d08ceb3161dd22
matiej <maciek.wojcik179@gmail.com>
2023-10-10 14:54:49 +0200

    implements full user resolver for user details. Works fine

---
+++ src/app/user/service/user.resolver.ts
@@ -0,0 +1,38 @@
+import { inject } from '@angular/core';
+import {
+    ActivatedRouteSnapshot,
+    ResolveFn,
+    RouterStateSnapshot,
+} from '@angular/router';
+import { Observable, catchError, map, of, take } from 'rxjs';
+import { User } from '../user-model';
+import { UserDatabaseService } from './user-database.service';
+
+export const userResolver: ResolveFn<User | null> = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot,
+  userDbService: UserDatabaseService = inject(UserDatabaseService)
+): Observable<User | null> => {
+  console.log('resolving user');
+  const id = route.paramMap.get('id');
+  if (id !== null && !isNaN(+id)) {
+    return userDbService.findById(+id).pipe(
+      map((item) => {
+        if (item) {
+          return item;
+        } else {
+          console.error('Item not found:', id);
+          return null;
+        }
+      }),
+      take(1),
+      catchError((error) => {
+        console.error('Error fetching wish item:', error);
+        return of(null);
+      })
+    );
+  } else {
+    console.error('Invalid ID:', id);
+    return of(null);
+  }
+};
