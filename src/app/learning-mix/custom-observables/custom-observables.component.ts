371ed04c1be0176bffd045348d0dd3e6e23af6c0
matiej <maciek.wojcik179@gmail.com>
2023-10-11 16:12:54 +0200

    create learning component to learn observables.

---
+++ src/app/learning-mix/custom-observables/custom-observables.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-observables',
+  templateUrl: './custom-observables.component.html',
+  styleUrls: ['./custom-observables.component.css']
+})
+export class CustomObservablesComponent {
+
+}
