98935c2e7aff45503f56452c49e8f3a8f9167e9c
matiej <maciek.wojcik179@gmail.com>
2023-10-19 14:31:29 +0200

    add section 15 component.

---
+++ src/app/learning-mix/section15-forms/section15-forms.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section15-forms',
+  templateUrl: './section15-forms.component.html',
+  styleUrls: ['./section15-forms.component.css']
+})
+export class Section15FormsComponent {
+
+}
