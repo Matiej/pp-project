566728d8d73b239fb86379fbfecba8035cff9ac7
matiej <maciek.wojcik179@gmail.com>
2023-10-19 14:32:51 +0200

    add example forms component in order to lern and train forms

---
+++ src/app/learning-mix/section15-forms/example-forms-view/example-forms-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example-forms-view',
+  templateUrl: './example-forms-view.component.html',
+  styleUrls: ['./example-forms-view.component.css']
+})
+export class ExampleFormsViewComponent {
+
+}
