6190a25b972925a74feed311745b5053bc462938
matiej <maciek.wojcik179@gmail.com>
2023-10-19 14:32:11 +0200

    add example-form1 component.

---
+++ src/app/learning-mix/section15-forms/example-form1/example-form1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example-form1',
+  templateUrl: './example-form1.component.html',
+  styleUrls: ['./example-form1.component.css']
+})
+export class ExampleForm1Component {
+
+}
