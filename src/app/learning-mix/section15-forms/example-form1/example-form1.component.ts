1e930b08e3c7a100e66931ed340bbf5209b58cf4
matiej <maciek.wojcik179@gmail.com>
2023-10-23 11:25:25 +0200

    example form1 implementation

--- src/app/learning-mix/section15-forms/example-form1/example-form1.component.ts
+++ src/app/learning-mix/section15-forms/example-form1/example-form1.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Section15ShareService } from '../section15-share.service';
+import { UserFormModel } from '../user.form.model';
 
 @Component({
   selector: 'app-example-form1',
   templateUrl: './example-form1.component.html',
-  styleUrls: ['./example-form1.component.css']
+  styleUrls: ['./example-form1.component.css'],
 })
 export class ExampleForm1Component {
+  readonly formTitle: string = 'Example of NgForm formlar';
 
+  constructor(private section15SharedService: Section15ShareService) {}
+
+  onSubmitUserForm(userForm: NgForm): void {
+    const form = userForm.value;
+    const userFormModel: UserFormModel = {
+      username: form.username,
+      email: form.email,
+      question: form.secret,
+    };
+    this.section15SharedService.sendUserFormToView(userFormModel);
+    
+  }
+
+  suggestUserName(): void {
+    const name = 'SuperUser';
+  }
 }
