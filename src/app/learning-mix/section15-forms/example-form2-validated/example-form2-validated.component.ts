a507f9c98546124da9ca6cde1d1795908b66ec4e
matiej <maciek.wojcik179@gmail.com>
2023-10-23 13:28:26 +0200

    add example form2 with validation. To learn validation and others.

---
+++ src/app/learning-mix/section15-forms/example-form2-validated/example-form2-validated.component.ts
@@ -0,0 +1,32 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Section15ShareService } from '../section15-share.service';
+
+@Component({
+  selector: 'app-example-form2-validated',
+  templateUrl: './example-form2-validated.component.html',
+  styleUrls: ['./example-form2-validated.component.css'],
+})
+export class ExampleForm2ValidatedComponent {
+  readonly formTitle: string = 'Example of NgForm formlar with Validation';
+  private _isSumbittButtonDisabeled: boolean = true;
+
+  @ViewChild('userForm')
+  userFrom!: NgForm;
+
+  constructor(private section15Service: Section15ShareService) {}
+
+  onSubmitUserForm() {
+    if (this.userFrom.valid) {
+      this.section15Service.sendUserFormToView(this.userFrom);
+      this.userFrom.reset();
+    }
+  }
+
+  public get isSumbittButtonDisabeled(): boolean {
+    if (this.userFrom && this.userFrom.valid) {
+      return false;
+    }
+    return true;
+  }
+}
