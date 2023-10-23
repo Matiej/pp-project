a6f89eed10ebc1f6722cbeb50e15c1cd6f5331af
matiej <maciek.wojcik179@gmail.com>
2023-10-23 11:24:54 +0200

    example form view component generated

--- src/app/learning-mix/section15-forms/example-forms-view/example-forms-view.component.ts
+++ src/app/learning-mix/section15-forms/example-forms-view/example-forms-view.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ASSETS_PATHS } from 'src/app/constants/assets-paths';
+import { Section15ShareService } from '../section15-share.service';
+import { UserFormModel } from '../user.form.model';
 
 @Component({
   selector: 'app-example-forms-view',
   templateUrl: './example-forms-view.component.html',
-  styleUrls: ['./example-forms-view.component.css']
+  styleUrls: ['./example-forms-view.component.css'],
 })
-export class ExampleFormsViewComponent {
+export class ExampleFormsViewComponent implements OnInit, OnDestroy {
+  readonly userImage: string = ASSETS_PATHS.USER_IMAGE_ICON;
+  private _formSubscription: Subscription = new Subscription();
+  user!: UserFormModel;
+  userCardTitle: string = 'No User details available';
+  constructor(private section15Service: Section15ShareService) {}
 
+  ngOnInit(): void {
+    this.section15Service.userFormSender.subscribe((user: UserFormModel) => {
+      this.userCardTitle = 'Submitted User details';
+      this.user = user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this._formSubscription) {
+      this._formSubscription.unsubscribe();
+    }
+  }
 }
