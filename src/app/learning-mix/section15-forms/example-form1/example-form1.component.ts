63f89b7c067bff3a25b4891b88b311cc8feab712
matiej <maciek.wojcik179@gmail.com>
2023-10-27 11:12:05 +0200

    add default values.

--- src/app/learning-mix/section15-forms/example-form1/example-form1.component.ts
+++ src/app/learning-mix/section15-forms/example-form1/example-form1.component.ts
@@ -9,15 +9,16 @@ import { Section15ShareService } from '../section15-share.service';
 })
 export class ExampleForm1Component {
   readonly formTitle: string = 'Example of NgForm formlar';
+  username: string = '';
 
   constructor(private section15SharedService: Section15ShareService) {}
 
   onSubmitUserForm(userForm: NgForm): void {
     this.section15SharedService.sendUserFormToView(userForm);
-    
   }
 
   suggestUserName(): void {
     const name = 'SuperUser';
+    this.username = name;
   }
 }
