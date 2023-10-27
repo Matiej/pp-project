5f7ba2dd73e916917d4c412c6e78b3951d7ab48f
matiej <maciek.wojcik179@gmail.com>
2023-10-27 11:15:33 +0200

    add gender place and validation

--- src/app/learning-mix/section15-forms/example-form2-validated/example-form2-validated.component.ts
+++ src/app/learning-mix/section15-forms/example-form2-validated/example-form2-validated.component.ts
@@ -9,18 +9,31 @@ import { Section15ShareService } from '../section15-share.service';
 })
 export class ExampleForm2ValidatedComponent {
   readonly formTitle: string = 'Example of NgForm formlar with Validation';
-  selectedSecret: string;
+
   questionAswer: string = '';
   @ViewChild('userForm')
   userFrom!: NgForm;
+  readonly genders: string[] = ['male', 'female', 'my secret'];
+  readonly defaultSecretQuestion: string = 'pet';
 
-  constructor(private section15Service: Section15ShareService) {
-    this.selectedSecret = 'pet';
+  constructor(private section15Service: Section15ShareService) {}
+
+  suggestUserName() {
+    console.log(this.userFrom);
+    this.userFrom.form.patchValue({
+      userData: {
+        username: 'SupeUserName',
+      },
+    });
+    this.userFrom.form.patchValue({
+      userSecret: {
+        secret: 'pet',
+      },
+    });
   }
 
   onSubmitUserForm() {
-    console.log(this.userFrom.value)
-    if (this.userFrom.valid) {
+    if (this.userFrom && this.userFrom.valid) {
       this.section15Service.sendUserFormToView(this.userFrom);
       this.userFrom.reset();
       this.questionAswer = '';
