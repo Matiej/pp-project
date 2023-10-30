63cadbebc1c360856e66a5f48e202aceec582411
matiej <maciek.wojcik179@gmail.com>
2023-10-30 16:26:38 +0100

    implement form, will be continue

--- src/app/learning-mix/example-reactive-form/example-reactive-form.component.ts
+++ src/app/learning-mix/example-reactive-form/example-reactive-form.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-example-reactive-form',
   templateUrl: './example-reactive-form.component.html',
   styleUrls: ['./example-reactive-form.component.css'],
 })
-export class ExampleReactiveFormComponent {
+export class ExampleReactiveFormComponent implements OnInit {
   genders: string[] = [];
   userForm!: FormGroup;
 
   constructor() {
     this.genders = ['male', 'female', 'unknown'];
+  }
+  ngOnInit(): void {
+     this.userForm = new FormGroup({
+      'usrername': new FormControl(null, Validators.required),
+      'email': new FormControl(null, Validators.required),
+      'genderInput': new FormControl('unknown', Validators.required)
+     });
   }
 
 }
