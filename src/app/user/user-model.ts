a49139c77e8e31ae55553a912675003ca0e3a01f
matiej <maciek.wojcik179@gmail.com>
2023-11-08 14:05:41 +0100

    add fileds to user model

--- src/app/user/user-model.ts
+++ src/app/user/user-model.ts
@@ -5,6 +5,16 @@ export class User {
   public email: string;
   public birthYear: number;
 
+  private _password!: string;
+  private _matchPassword!: string;
+  private _address1!: string;
+  private _address2!: string;
+  private _gender!: string;
+  private _secret!: string;
+  private _answer!: string;
+  private _about!: string;
+  
+
   constructor(
     name: string,
     lastName: string,
@@ -23,5 +33,68 @@ export class User {
 
   set id(value: number) {
     this._id = value;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  set password(value: string) {
+    this._password = value;
+  }
+
+  get matchPassword(): string {
+    return this._matchPassword;
+  }
+
+  set matchPassword(value: string) {
+    this._matchPassword = value;
+  }
+
+  get address1(): string {
+    return this._address1;
+  }
+
+  set address1(value: string) {
+    this._address1 = value;
+  }
+
+  get address2(): string {
+    return this._address2;
+  }
+
+  set address2(value: string) {
+    this._address2 = value;
+  }
+
+  get gender(): string {
+    return this._gender;
+  }
+
+  set gender(value: string) {
+    this._gender = value;
+  }
+
+  get secret(): string {
+    return this._secret;
+  }
+
+  set secret(value: string) {
+    this._secret = value;
+  }
+
+  get answer(): string {
+    return this._answer;
+  }
+
+  set answer(value: string) {
+    this._answer = value;
+  }
+
+  public get about(): string {
+    return this._about;
+  }
+  public set about(value: string) {
+    this._about = value;
   }
 }
