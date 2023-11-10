e8154aa297845f0b625633db55fb423ed34b94c3
matiej <maciek.wojcik179@gmail.com>
2023-11-10 15:23:05 +0100

    adding section17 learning component.

---
+++ src/app/learning-mix/section17-pipes/section17-pipes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section17-pipes',
+  templateUrl: './section17-pipes.component.html',
+  styleUrls: ['./section17-pipes.component.css']
+})
+export class Section17PipesComponent {
+
+}
