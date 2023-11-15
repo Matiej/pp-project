1159740fec58b1e63b16fabce587bd8550ed69ad
matiej <maciek.wojcik179@gmail.com>
2023-11-15 15:57:45 +0100

    new component for section 18

---
+++ src/app/learning-mix/section18-http-req/section18-http-req.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section18-http-req',
+  templateUrl: './section18-http-req.component.html',
+  styleUrls: ['./section18-http-req.component.css']
+})
+export class Section18HttpReqComponent {
+
+}
