658d06f82eb352d7f323a16316dcd5c4e8e0d1a3
matiej <maciek.wojcik179@gmail.com>
2023-11-23 12:35:50 +0100

    add spinner and teoast messages to login component.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { TOAST_MESSAGES } from 'src/app/constants/toast-messages';
 import { UserDatabaseService } from 'src/app/user/service/user-database.service';
 import { User } from 'src/app/user/user-model';
 import { AuthService } from '../auth.service';
@@ -17,6 +19,10 @@ export class LoginComponent implements OnInit, OnDestroy {
   loginForm!: FormGroup;
   isLoginError: boolean = false;
   private loginObservale?: Subscription;
+  isSpinning: boolean = false;
+  showToast: boolean = false;
+  toastMessageClass: string = '';
+  userToastMessage: string = '';
 
   constructor(
     private authService: AuthService,
@@ -38,6 +44,7 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   onUserLogin(): void {
+    this.isSpinning = true;
     const formData = this.loginForm.value;
     this.loginObservale = this.userDbService
       .findByEmail(formData.email)
@@ -48,24 +55,68 @@ export class LoginComponent implements OnInit, OnDestroy {
             this.authService.isAuthenticated().then((auth: boolean) => {
               this.isLoggedIn = auth;
               if (this.isLoggedIn) {
+                this.showToastMessage(
+                  TOAST_MESSAGES.LOGGED_SUCCESSFULLY,
+                  2500,
+                  TOAST_MESSAGES.SUCCESS_MESSAGE_STYLE
+                );
                 this.user = user;
                 this.sharedAuthService.userLoggedINNotification();
+                this.isSpinning = false;
+                this.loginForm.reset();
               } else {
+                this.showToastMessage(
+                  TOAST_MESSAGES.WRONG_USERNAME_OR_PASS,
+                  2500,
+                  TOAST_MESSAGES.DANGER_MESSAGE_STYLE
+                );
+                this.loginForm.reset();
+                this.isSpinning = false;
                 console.error('User is not logged in - wrong password');
                 this.isLoginError = true;
               }
             });
           } else {
+            this.showToastMessage(
+              TOAST_MESSAGES.WRONG_USERNAME_OR_PASS,
+              2500,
+              TOAST_MESSAGES.DANGER_MESSAGE_STYLE
+            );
+            this.loginForm.reset();
+            this.isSpinning = false;
             console.error('User not found');
             this.isLoginError = true;
           }
         },
-        (error: Error) => console.error(error),
-        () => this.loginForm.reset()
+        (error: HttpErrorResponse) => {
+          this.showToastMessage(
+            TOAST_MESSAGES.ERROR_LOGGING + ' --- Server error: ' + error.error.error,
+            4000,
+            TOAST_MESSAGES.DANGER_MESSAGE_BIG_STYLE
+          );
+          this.isSpinning = false;
+          console.error(error);
+          this.loginForm.reset();
+        }
       );
   }
 
   getUserStatus(): string {
     return this.isLoggedIn ? 'logged in' : 'not logged in';
+  }
+
+  private showToastMessage(
+    message: string,
+    timeout: number,
+    messageStyle: string
+  ): void {
+    this.userToastMessage = message;
+    this.showToast = true;
+    this.toastMessageClass = messageStyle;
+    setTimeout(() => {
+      this.showToast = false;
+      this.userToastMessage = '';
+      this.toastMessageClass = '';
+    }, timeout);
   }
 }
