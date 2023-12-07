8e770292f542fb420a0cda6a9ed967f6c55e79e6
matiej <maciek.wojcik179@gmail.com>
2023-12-07 09:55:59 +0100

    remove wrong field form interaface

--- src/app/auth/authResponse.ts
+++ src/app/auth/authResponse.ts
@@ -1,8 +1,7 @@
 export interface AuthResponseData {
-    kind: string;
-    idToken: string;
-    email:  string;
-    refreshToken: string;
-    expiresIn: string;
-    localId: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
 }
