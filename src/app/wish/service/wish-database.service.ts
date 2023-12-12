23bff66e3bca7d41049fb15dd7115cffd3398e34
matiej <maciek.wojcik179@gmail.com>
2023-12-12 16:12:23 +0100

    move database logic from inmemory database to firebase for wishes

--- src/app/wish/service/wish-database.service.ts
+++ src/app/wish/service/wish-database.service.ts
@@ -1,36 +1,87 @@
+import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { InMemoryWishItemDataBase } from '../db/in-memory-wishItem-database';
+import { Observable, map, switchMap } from 'rxjs';
+import { FirebaseWishDatabaseService } from '../db/firebase-wish-database.service';
+
 import { WishItem } from '../wish-list/wish-item/wish-item-model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WishDatabaseService {
-  private _whisItemDatabase!: InMemoryWishItemDataBase;
+  // private _whisItemDatabase!: InMemoryWishItemDataBase;
 
-  constructor() {
-    this._whisItemDatabase = new InMemoryWishItemDataBase();
+  constructor(
+    private firebaseWishDatabaseService: FirebaseWishDatabaseService
+  ) {
+    // this._whisItemDatabase = new InMemoryWishItemDataBase();
   }
 
-  saveWishItem(item: WishItem): WishItem | undefined {
-    return this._whisItemDatabase.add(item);
+  // saveWishItem(item: WishItem): WishItem | undefined {
+  //   return this._whisItemDatabase.add(item);
+  // }
+
+  saveWish(wish: WishItem): Observable<WishItem | undefined> {
+    return this.firebaseWishDatabaseService.saveWish(wish).pipe(
+      switchMap((savedWishID: { name: string }) => {
+        return this.findWishByid(savedWishID.name);
+      })
+    );
   }
 
-  findById(id: number): Observable<WishItem | undefined> {
-    return of(this._whisItemDatabase.get(id.toString()));
+  public findWishByid(wishId: string): Observable<WishItem | undefined> {
+    return this.firebaseWishDatabaseService.findWishById(wishId).pipe(
+      map((resposne: HttpResponse<any>) => {
+        if (resposne.status === 200) {
+          return this.convertToWishItem(resposne.body, wishId);
+        } else {
+          return undefined;
+        }
+      })
+    );
   }
 
   findAll(): Observable<WishItem[]> {
- 
-    return of(this._whisItemDatabase.listAll());
+    return this.firebaseWishDatabaseService.findAllWishes().pipe(
+      map((response) => {
+        const wishies: WishItem[] = [];
+        for (const key in response) {
+          if (response.hasOwnProperty(key)) {
+            const wish = this.convertToWishItem(response[key], key);
+            wishies.push(wish);
+          }
+        }
+        return wishies;
+      })
+    );
   }
 
-  removeById(id: number): boolean {
-    return this._whisItemDatabase.remove(id.toString());
+  removeById(id: string): Observable<boolean> {
+   return this.firebaseWishDatabaseService.deleteWishById(id)
+   .pipe(map((resposne: HttpResponse<any>) => {
+      if(resposne.status ===200) {
+        return true;
+      } else {
+        return false;
+      }
+   }));
+     
+ 
   }
 
   getNumberOfItems(): number {
-    return this._whisItemDatabase.listAll().length;
+    return this.firebaseWishDatabaseService.findAllWishes.length;
+  }
+
+  private convertToWishItem(wishData: any, key: string): WishItem {
+    const wishItem = new WishItem(
+      wishData.name,
+      wishData.type,
+      wishData.descriptions,
+      wishData.pictureUrl
+    );
+    wishItem.id = key;
+
+    return wishItem;
   }
 }
