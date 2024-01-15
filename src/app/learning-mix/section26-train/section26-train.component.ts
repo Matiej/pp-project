9f49e8226d9795360f9447277abc4a388154f4ea
matiej <maciek.wojcik179@gmail.com>
2024-01-15 09:57:43 +0100

    ad new learning component.

---
+++ src/app/learning-mix/section26-train/section26-train.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section26-train',
+  templateUrl: './section26-train.component.html',
+  styleUrls: ['./section26-train.component.css']
+})
+export class Section26TrainComponent {
+
+}
