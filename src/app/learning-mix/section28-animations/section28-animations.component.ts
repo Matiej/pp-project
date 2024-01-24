a12aeb7010152589f7d50d9214ed3195961b51e0
matiej <maciek.wojcik179@gmail.com>
2024-01-24 12:07:26 +0100

    add learning component for animations

---
+++ src/app/learning-mix/section28-animations/section28-animations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section28-animations',
+  standalone: true,
+  imports: [],
+  templateUrl: './section28-animations.component.html',
+  styleUrl: './section28-animations.component.css'
+})
+export class Section28AnimationsComponent {
+
+}
